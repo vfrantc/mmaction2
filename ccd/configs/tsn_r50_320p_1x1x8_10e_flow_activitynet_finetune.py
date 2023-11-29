fb4230edc91b3abbf7c5b5adc5e0c025ae679a48
Vladimir Frants <vfrants@gradcenter.cuny.edu>
2023-11-29 16:49:43 -0500

    flow

--- ccd/configs/tsn_r50_320p_1x1x8_10e_flow_activitynet_finetune.py
+++ ccd/configs/tsn_r50_320p_1x1x8_10e_flow_activitynet_finetune.py
@@ -15,7 +15,7 @@ data_root_val = 'data/ActivityNet/rawframes'
 ann_file_train = 'data/ActivityNet/anet_train_video.txt'
 ann_file_val = 'data/ActivityNet/anet_val_video.txt'
 ann_file_test = 'data/ActivityNet/anet_val_clip.txt'
-img_norm_cfg = dict(mean=[128, 128], std=[128, 128], to_bgr=False)
+img_norm_cfg = dict(mean=[128, 128], std=[128, 128])
 train_pipeline = [
     dict(type='SampleFrames', clip_len=5, frame_interval=1, num_clips=8),
     dict(type='RawFrameDecode'),
