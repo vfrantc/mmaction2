5af3ca2b7c4b6b4f279d0010223df003c5af20bb
Vladimir Frants <vfrants@gradcenter.cuny.edu>
2023-11-29 15:22:33 -0500

    configs

--- ccd/configs/tsn_r50_320p_1x1x8_50e_activitynet_video_rgb_vlad.py
+++ ccd/configs/tsn_r50_320p_1x1x8_50e_activitynet_video_rgb_vlad.py
@@ -7,11 +7,11 @@ _base_ = [
     '/content/mmaction2/configs/_base_/default_runtime.py'
 ]
 # model settings
-model = dict(cls_head=dict(num_classes=2, dropout_ratio=0.8))
+model = dict(cls_head=dict(num_classes=2, dropout_ratio=0.8)) # We inherit from tsn_r50 and change the number of classes, dropout_ratio is rather high
 
 # dataset settings
-dataset_type = 'RawframeDataset'
-data_root = './data/ActivityNet/rawframes'
+dataset_type = 'RawframeDataset' # VideoDataset for videos
+data_root = './data/ActivityNet/rawframes' #
 data_root_val = './data/ActivityNet/rawframes'
 ann_file_train = './data/ActivityNet/anet_train_video.txt'
 ann_file_val = './data/ActivityNet/anet_val_video.txt'
@@ -20,15 +20,15 @@ ann_file_test = './data/ActivityNet/anet_val_video.txt'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375])
 train_pipeline = [
-    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8),
-    dict(type='RawFrameDecode'),
-    dict(type='Resize', scale=(-1, 256)),
-    dict(type='RandomResizedCrop'),
-    dict(type='Resize', scale=(224, 224), keep_ratio=False),
-    dict(type='Flip', flip_ratio=0.5),
+    dict(type='SampleFrames', clip_len=1, frame_interval=1, num_clips=8), # Will it work for the raw frames??? Instead of the video
+    dict(type='RawFrameDecode'), # Decode individual image
+    dict(type='Resize', scale=(-1, 256)), # Resize to 256 in each demension?
+    dict(type='RandomResizedCrop'), # crop ???
+    dict(type='Resize', scale=(224, 224), keep_ratio=False), # So the input is 224x224 which was standard for resnet50
+    dict(type='Flip', flip_ratio=0.5), # randomly flip it
     # dict(type='Normalize', **img_norm_cfg),
-    dict(type='FormatShape', input_format='NCHW'),
-    dict(type='PackActionInputs'),
+    dict(type='FormatShape', input_format='NCHW'), # batch x 3 x 224 x 224 in our case
+    dict(type='PackActionInputs'), #
     # Removed 'Collect' and 'ToTensor' to align with the working config
 ]
 val_pipeline = [
