072a02bd7e1a3a8b78af730fc07c0e2b5747f5a4
Vladimir Frants <vfrants@gradcenter.cuny.edu>
2023-12-01 07:05:56 -0500

    fixed frame counter

--- tools/data/activitynet/generate_rawframes_filelist.py
+++ tools/data/activitynet/generate_rawframes_filelist.py
@@ -7,37 +7,45 @@ data_file = './data/ActivityNet'
 video_list = f'{data_file}/video_info_new.csv'
 anno_file = f'{data_file}/anet_anno_action.json'
 rawframe_dir = f'{data_file}/rawframes'
-action_name_list = './tools/data/activitynet/action_name.csv'
+action_name_list = './tools/data/activitynet/action_name.csv' # Two in our case
 
+# Rawframes are in the same folder but there are sets for frame : flowx : flowy
 train_rawframe_dir = rawframe_dir
 val_rawframe_dir = rawframe_dir
 
+# The dataset labeling
 json_file = f'{data_file}/activity_net.v1-3.min.json'
 
 
 def generate_rawframes_filelist():
+    # get the list of files
+    # Load the .json file
     load_dict = json.load(open(json_file))
-
+    # other file with the same information I guess
     anet_labels = open(action_name_list).readlines()
-    anet_labels = [x.strip() for x in anet_labels[1:]]
+    anet_labels = [x.strip() for x in anet_labels[1:]] # skip the first one, as it is the csv file and it is just the category name
 
     train_dir_list = [
-        osp.join(train_rawframe_dir, x) for x in os.listdir(train_rawframe_dir)
+        osp.join(train_rawframe_dir, x) for x in os.listdir(train_rawframe_dir) # get filenames for training part
     ]
     val_dir_list = [
-        osp.join(val_rawframe_dir, x) for x in os.listdir(val_rawframe_dir)
+        osp.join(val_rawframe_dir, x) for x in os.listdir(val_rawframe_dir) # get frames for the validation
     ]
 
     def simple_label(anno):
-        label = anno[0]['label']
+        label = anno[0]['label'] # extract the label and convert it to the index: in our case that just 0 or 1
         return anet_labels.index(label)
 
     def count_frames(dir_list, video):
-        for dir_name in dir_list:
-            if video in dir_name:
-                print(f"Counting files from: {dir_name}")
-                rgb_filtered_files = [f for f in os.listdir(dir_name) if not f.startswith("flow")]
-                return osp.basename(dir_name), len(rgb_filtered_files)
+        for dir_name in dir_list: # go trough all the dirs
+            if video in dir_name: # and then vides???
+                #print(f"Counting files from: {dir_name}")
+                files = os.listdir(dir_name)
+                img_files_num = len([f for f in files if f.startswith('img_')])
+                flowx_files_num = len([f for f in files if f.startswith('flow_x')])
+                flowy_files_num = len([f for f in files if f.startswith('flow_y')])
+                num_frames = min([img_files_num, flowx_files_num, flowy_files_num])
+                return osp.basename(dir_name), num_frames
         return None, None
 
     database = load_dict['database']
--- tools/data/activitynet/tsn_extract_flow_feat_config.py
+++ tools/data/activitynet/tsn_extract_flow_feat_config.py
@@ -1,16 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 _base_ = [
-    'mmaction::_base_/models/tsn_r50.py', 'mmaction::_base_/default_runtime.py'
+    'mmaction::_base_/models/tsn_r50.py', 'mmaction::_base_/default_runtime.py' # so it uses tsn and is not designed for training
 ]
 
-clip_len = 5
+clip_len = 5 # works on clips of size of 5
 model = dict(
-    backbone=dict(in_channels=2 * clip_len),
-    data_preprocessor=dict(mean=[128], std=[128]))
+    backbone=dict(in_channels=2 * clip_len), # it will apply the same feature extraction to set of channels, vertical and horizontal
+    data_preprocessor=dict(mean=[128], std=[128])) # subtract mean and devide by std
 
 # dataset settings
-dataset_type = 'RawframeDataset'
-data_root_val = 'data/kinetics400/rawframes_val'
+dataset_type = 'RawframeDataset' # RawframeDataset
+data_root_val = 'data/kinetics400/rawframes_val' # here the f
 ann_file_val = 'data/kinetics400/kinetics400_val_list_rawframes.txt'
 
 file_client_args = dict(io_backend='disk')
